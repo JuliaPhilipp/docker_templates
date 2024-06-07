3ed7fe341b6f083caa97113d873e0a9f8b5ab278
Julia Philipp <julia.philipp@embl.de>
2024-06-07 15:19:00 +0200

    update scrap_rclone

--- cron_test/scrap_rclone.sh
+++ cron_test/scrap_rclone.sh
@@ -1,3 +1,4 @@
+
 # scrap for rclone stuff
 rclone ls DKFZ-DECODE:erc-project/decodedb_backup/211001
 
@@ -24,4 +25,69 @@ PSQL_HOST="db"
 rclone ls DKFZ-DECODE:erc-project/raw_data/seq_runs/DSR-00011/211119_NB552290_0153_AHL5MLBGXK/211119_NB552290_0153_AHL5MLBGXK/Alignment_1/20211120_120450/Fastq/
 rclone ls DKFZ-DECODE:erc-project/raw_data/DM/DS-00151
 
-rclone copy DKFZ-DECODE:erc-project/raw_data/seq_runs/DSR-00011/211119_NB552290_0153_AHL5MLBGXK/211119_NB552290_0153_AHL5MLBGXK/Alignment_1/20211120_120450/Fastq/SEQ366TX271ctrl_S1_L001_R2_001.fastq.gz DKFZ-DECODE:erc-project/raw_data/DM/DS-00151
+
+
+rclone copy DKFZ-DECODE:erc-project/raw_data/DM/DS-00131/drosophila_gut/017098_all_cellranger_output/matrix.mtx ~/Downloads/ --dry-run
+rclone copy DKFZ-DECODE:erc-project/raw_data/DM/DS-00131/drosophila_gut/017098_all_cellranger_output/matrix.mtx Downloads
+
+rclone ls DKFZ-DECODE:erc-project/raw_data/DM/DS-00131/drosophila_gut/017098_all_cellranger_output/matrix.mtx 
+
+## clean up TX22 and TX23 sequencing run data (there are additional copies of the fastqs saved there)
+rclone ls DKFZ-DECODE:erc-project/raw_data/seq_runs/DSR-00009 --include '1LongGEXd*'
+rclone delete DKFZ-DECODE:erc-project/raw_data/seq_runs/DSR-00009 --include '1LongGEXd*'
+
+rclone copy DKFZ-DECODE:erc-project/raw_data/DM/DS-000C5/objects/DS-000C5.rds ~/Downloads/
+
+rclone ls DKFZ-DECODE:erc-project/raw_data/DM/DS-00001
+
+rclone copy DKFZ-DECODE:erc-project/raw_data/DM/DS-0017E/analysis/DS-0017E_filtered_srt_log_cluster.rds DKFZ-DECODE:erc-project/raw_data/DM/DS-00001/analysis/DS-0017E_filtered_srt_log_cluster.rds
+
+rclone ls DKFZ-DECODE:erc-project/decodedb_backup/ --include '2112*'
+
+rclone copy DKFZ-DECODE:erc-project/raw_data/DM/DS-00188/counts/outs --include 
+
+rclone copy 230324_NB552290_0227_AH5WFVBGXT DKFZ-DECODE:erc-project/raw_data/seq_runs/DSR-00022/230324_NB552290_0227_AH5WFVBGXT/ --dry-run 
+rclone copy 230324_NB552290_0227_AH5WFVBGXT DKFZ-DECODE:erc-project/raw_data/seq_runs/DSR-00022/230324_NB552290_0227_AH5WFVBGXT/ --progress 
+
+
+
+ rclone ls DKFZ-DECODE:erc-project/raw_data/seq_runs/DSR-00030 --include 'SampleSheet*'
+
+rclone ls DKFZ-DECODE:erc-project/raw_data/DM/DSR-00022/
+
+
+## clean up organoid data
+rclone ls DKFZ-DECODE:erc-project/raw_data/DM/DS-00221 
+rclone purge DKFZ-DECODE:erc-project/raw_data/DM/DS-00221 --dry-run
+
+rclone ls DKFZ-DECODE:erc-project/raw_data/DM/DS-00220
+rclone delete DKFZ-DECODE:erc-project/raw_data/DM/DS-00220 
+
+rclone ls DKFZ-DECODE:erc-project/raw_data/seq_runs/DSR-00027/
+
+rclone delete DKFZ-DECODE:erc-project/raw_data/seq_runs/DSR-00027/ --dry-run
+rclone rmdir DKFZ-DECODE:erc-project/raw_data/seq_runs/DSR-00027/ --dry-run
+
+rclone ls DKFZ-DECODE:erc-project/raw_data/seq_runs/DSR-00028
+rclone purge  DKFZ-DECODE:erc-project/raw_data/seq_runs/DSR-00028 --dry-run
+
+rclone ls DKFZ-DECODE:erc-project/raw_data/seq_runs/DSR-0002C
+rclone purge  DKFZ-DECODE:erc-project/raw_data/seq_runs/DSR-0002C --dry-run
+
+rclone ls DKFZ-DECODE:erc-project/raw_data/seq_runs/DSR-0002A
+rclone purge  DKFZ-DECODE:erc-project/raw_data/seq_runs/DSR-0002A --dry-run	
+
+# clean lohmanlab data
+rclone ls DKFZ-DECODE:erc-project/raw_data/seq_runs/DSR-0002B
+rclone delete DKFZ-DECODE:erc-project/raw_data/seq_runs/DSR-0002B --dry-run
+
+
+## find decode data
+
+s3://erc-project/data_share/20240115_pb/
+
+rclone ls DKFZ-DECODE:erc-project/data_share/20240115_pb/
+
+rclone copy DKFZ-DECODE:erc-project/data_share/20240115_pb/ ~/Downloads/
+
+
